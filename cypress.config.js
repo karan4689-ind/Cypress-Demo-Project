c33dbc036e31acb8b66efcbb2fdb2d6234613377
karan4689-ind <karansharma4689@gmail.com>
2026-05-09 22:10:10 +0530

    Implemented Mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   env: {
