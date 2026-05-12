1b4128b5b4f8374ce6d69aa231810db6e33615a6
karan4689-ind <karansharma4689@gmail.com>
2026-05-12 17:24:24 +0530

    Integration with Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  projectId: "h6yg6x",
 
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
